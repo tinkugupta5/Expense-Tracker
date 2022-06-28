f0c74407f3440cbbfb088883545b91d0a220202e
tinku gupta <tinku.gupta4@gmail.com>
2022-06-28 10:46:06 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const Header = () => {
+  return (
+    <div>Expense Tracker</div>
+    
+  )
+}
+
+export default Header
---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function incomeExpenses() {
+  return (
+    <div>incomeExpenses</div>
+  )
+}
