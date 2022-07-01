7fd3d9bb0d702995149b00e024bd68192b5e599f
tinku gupta <tinku.gupta4@gmail.com>
2022-07-01 05:48:16 +0530

    Add state to to the inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Balance } from './components/Balance';
 import './App.css';
 import Expenses from './components/Expenses';
 import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Balance />
         <Expenses/>
         <TransactionList/>
+        <AddTransaction/>
+
       </div>
     </>
 
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,7 +5,7 @@ export default function TransactionList() {
     <>
     
     <h3>History</h3>
-    <ul id="list"  className="list">
+    <ul className="list">
       <li  className="minus">
         Cash <span>-$400</span><button  className="delete-btn">x</button>
       </li> 
