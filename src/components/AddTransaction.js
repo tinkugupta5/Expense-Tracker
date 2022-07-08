f47358496a33825bef8ce6faf3e6e98d0c92b37c
tinku gupta <tinku.gupta4@gmail.com>
2022-07-08 23:47:22 +0530

    thisis formated changes

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,12 +10,12 @@ export default function AddTransaction() {
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e)=>setText(e.target.value)}  placeholder="Enter text..." />
+        
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+          <label htmlFor="amount">Amount <br />
+            (negative - expense, positive - income)</label>
+          
           <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)}  placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
