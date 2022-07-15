69f932b9de7c37ca803c8d36279ec4f86f6b0930
tinku gupta <tinku.gupta4@gmail.com>
2022-07-15 23:53:47 +0530

    just change in tghe souce code

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -3,13 +3,15 @@ import React from 'react'
 function IncomeExpenses() {
   return (
     <div className="inc-exp-container">
+      
         <div>
           <h4>Income</h4>
           <p className="money plus"></p>
         </div>
+
         <div>
           <h4>Expense</h4>
-       <p className="money minus"></p>
+           <p className="money minus"></p>
         </div>
     </div>
   )
